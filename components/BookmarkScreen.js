bc27ddd8e29741062e0dc663c765c6f0e1a54097
clementgyimah <clementgyimah2@gmail.com>
2020-02-03 00:45:10 +0000

    Making Realm accessible in differnt files

--- components/BookmarkScreen.js
+++ components/BookmarkScreen.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, StyleSheet } from 'react-native';
+import { FlatList, TouchableHighlight } from 'react-native-gesture-handler';
+import SearchScreen from './SearchScreen';
 
-export default class BookmarkScreen extends Component {
-  constructor(props) {
+const Realm = require('realm');
+
+savedWords = [
+  {word: "a", meaning: "To greet someone..."},
+  {word:  "aa", meaning: "That is my originall ..."},
+  {word: "aaa", meaning: "That is my nick..."},
+  {word: "aah", meaning: "That is my sir..."}
+]
+
+
+export default class BookmarkScreen extends Component{
+
+  constructor(props){
     super(props);
     this.state = {
-    };
+      text: ""
+    }
   }
 
-  render() {
-    return (
-      <View>
-        <Text> BookmarkScreen </Text>
-      </View>
-    );
+  componentDidMount(){
+
   }
+
+
+
+  render(){
+  return(
+    <View style={styles.main}>
+    <FlatList 
+    data={savedWords}
+    renderItem={({item}) => (
+        <TouchableHighlight style={styles.container} /*onPress={()=> this.props.navigation.navigate("SearchScreen", {searchword: savedWords.word})}*/ >
+          <View>
+        <Text style={styles.nametxt}>{item.word}</Text>
+        <Text style={styles.descriptiontxt}>{item.meaning}</Text>
+        </View>
+        </TouchableHighlight>
+    )}
+    />
+    </View>
+  )
+    }
 }
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'yellow'
+  },
+  container: {
+    backgroundColor: 'blue',
+    margin: 7.0,
+    borderRadius: 5.0
+  }, 
+  nametxt: {
+    fontSize: 40.0,
+    fontFamily: 'bold',
+    marginLeft: 5.0,
+    color: 'white'
+  }, 
+  descriptiontxt: {
+    fontSize: 20.0,
+    fontFamily: "h",
+    marginLeft: 5.0,
+    color: 'white'
+  }, 
+})
